423d4284a8725b0bc0aab8aefef92da52fafd489
jinhyeong <puase7@naver.com>
2025-09-22 10:54:08 +0900

    2025_09_22 cv_practice

---
+++ ch4/4-3.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+img=cv.imread(r"C:\Users\kim\cv_practice\ch3\soccer.jpg")
+gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+canny=cv.Canny(gray,100,200)
+
+contour,hierarchy=cv.findContours(canny,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
+
+
+lcontour=[]
+for i in range(len(contour)):
+    if contour[i].shape[0]>100:
+        lcontour.append(contour[i])
+
+
+cv.drawContours(img,lcontour,-1,(0,255,0),2)
+
+cv.imshow('Original with contours',img)
+cv.imshow('Canny',canny)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
